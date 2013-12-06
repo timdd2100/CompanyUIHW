12a751682cf6bf4b686bdec17b5936491a0a70aa
timdd2100 <timkao@sklteki-MacBook-Pro.local>
2013-12-06 10:27:36 +0800

    Initial Commit

---
+++ CompanyUIHW/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CompanyUIHW
+//
+//  Created by timkao on 13/12/6.
+//  Copyright (c) 2013年 skl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
